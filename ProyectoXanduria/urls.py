155570a081bde8d23ef51b49ee386a1a333a6b82
Javier.Martinez <soporte.javier@outlook.com>
2024-09-19 14:11:02 -0500

    actualizacion de codigo en urls

--- ProyectoXanduria/urls.py
+++ ProyectoXanduria/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Principalapp.urls')),
-    path('contactenos', include('Contactenos.urls')),
-    path('registro', include('registro.urls')),
+    path('', include('Contactenos.urls')),
+    path('', include('registro.urls')),
     
 ]
